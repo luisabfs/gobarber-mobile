e5e13140e4dac3b9fddf9f3b9f0a09cef8d6b9b0
Luisa Barros <qlullyq@gmail.com>
2020-11-19 00:00:54 -0300

    chore: add react-navigation lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+
 import React from 'react';
-import {View} from 'react-native';
+import { View, StatusBar } from 'react-native';
 
-// import { Container } from './styles';
-
-const App: React.FC = () => {
-  return <View />;
-};
+const App: React.FC = () => (
+  <NavigationContainer>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }} />
+  </NavigationContainer>
+);
 
 export default App;
