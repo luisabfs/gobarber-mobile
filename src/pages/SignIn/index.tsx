bf655d6e2d5f63c628426265b2fa037091168626
Luísa Barros <qlullyq@gmail.com>
2020-11-20 14:42:43 -0300

    feat: add Button and Input to SignIn page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
+import { Input, Button } from '../../components';
 
 import logo from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => (
   <Container>
     <Image source={logo} />
     <Title>Login</Title>
+
+    <Input name="email" icon="mail" placeholder="Email" />
+    <Input name="password" icon="lock" placeholder="Password" />
+
+    <Button onPress={() => console.log('Button')}>Sign in</Button>
   </Container>
 );
 
