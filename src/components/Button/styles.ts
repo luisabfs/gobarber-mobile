e1c86349219eb801ff1549975db9ff7ac0ae4162
Luísa Barros <qlullyq@gmail.com>
2020-11-20 15:22:43 -0300

    feat: create SignUp page interface

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -6,7 +6,7 @@ export const Container = styled(RectButton)`
   align-items: center;
 
   width: 100%;
-  height: 60px;
+  height: 50px;
   background: #ff9000;
   border-radius: 10px;
   margin-top: 10px;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,22 +1,58 @@
 import React from 'react';
-import { Image } from 'react-native';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/Feather';
 
 import { Input, Button } from '../../components';
 
 import logo from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPasswordButton,
+  ForgotPasswordText,
+  SignUpButton,
+  SignUpText,
+} from './styles';
 
 const SignIn: React.FC = () => (
-  <Container>
-    <Image source={logo} />
-    <Title>Login</Title>
+  <>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      enabled
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <Container>
+          <Image source={logo} />
+          <Title>Login</Title>
 
-    <Input name="email" icon="mail" placeholder="Email" />
-    <Input name="password" icon="lock" placeholder="Password" />
+          <Input name="email" icon="mail" placeholder="Email" />
+          <Input name="password" icon="lock" placeholder="Password" />
 
-    <Button onPress={() => console.log('Button')}>Sign in</Button>
-  </Container>
+          <Button onPress={() => console.log('Sign in')}>Sign in</Button>
+
+          <ForgotPasswordButton onPress={() => console.log('Button')}>
+            <ForgotPasswordText>Forgot password</ForgotPasswordText>
+          </ForgotPasswordButton>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+
+    <SignUpButton onPress={() => console.log('Sign up')}>
+      <Icon name="log-in" size={20} color="#ff9000" />
+      <SignUpText>Create account</SignUpText>
+    </SignUpButton>
+  </>
 );
 
 export default SignIn;
