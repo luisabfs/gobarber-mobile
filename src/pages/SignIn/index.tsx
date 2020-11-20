80b48f12b3650695dd101fd64f66463444518a60
Luísa Barros <qlullyq@gmail.com>
2020-11-20 16:04:10 -0300

    feat: add navigation to SignIn and SignUp pages

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -6,6 +6,8 @@ import {
   ScrollView,
 } from 'react-native';
 
+import { useNavigation } from '@react-navigation/native';
+
 import Icon from 'react-native-vector-icons/Feather';
 
 import { Input, Button } from '../../components';
@@ -21,38 +23,42 @@ import {
   SignUpText,
 } from './styles';
 
-const SignIn: React.FC = () => (
-  <>
-    <KeyboardAvoidingView
-      style={{ flex: 1 }}
-      enabled
-      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-    >
-      <ScrollView
-        keyboardShouldPersistTaps="handled"
-        contentContainerStyle={{ flex: 1 }}
+const SignIn: React.FC = () => {
+  const navigation = useNavigation();
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        enabled
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
       >
-        <Container>
-          <Image source={logo} />
-          <Title>Login</Title>
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logo} />
+            <Title>Login</Title>
 
-          <Input name="email" icon="mail" placeholder="Email" />
-          <Input name="password" icon="lock" placeholder="Password" />
+            <Input name="email" icon="mail" placeholder="Email" />
+            <Input name="password" icon="lock" placeholder="Password" />
 
-          <Button onPress={() => console.log('Sign in')}>Sign in</Button>
+            <Button onPress={() => console.log('Sign in')}>Sign in</Button>
 
-          <ForgotPasswordButton onPress={() => console.log('Button')}>
-            <ForgotPasswordText>Forgot password</ForgotPasswordText>
-          </ForgotPasswordButton>
-        </Container>
-      </ScrollView>
-    </KeyboardAvoidingView>
+            <ForgotPasswordButton onPress={() => console.log('Button')}>
+              <ForgotPasswordText>Forgot password</ForgotPasswordText>
+            </ForgotPasswordButton>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
 
-    <SignUpButton onPress={() => console.log('Sign up')}>
-      <Icon name="log-in" size={20} color="#ff9000" />
-      <SignUpText>Create account</SignUpText>
-    </SignUpButton>
-  </>
-);
+      <SignUpButton onPress={() => navigation.navigate('SignUp')}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <SignUpText>Create account</SignUpText>
+      </SignUpButton>
+    </>
+  );
+};
 
 export default SignIn;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -6,6 +6,8 @@ import {
   ScrollView,
 } from 'react-native';
 
+import { useNavigation } from '@react-navigation/native';
+
 import Icon from 'react-native-vector-icons/Feather';
 
 import { Input, Button } from '../../components';
@@ -19,35 +21,39 @@ import {
   BackToSignInText,
 } from './styles';
 
-const SignUp: React.FC = () => (
-  <>
-    <KeyboardAvoidingView
-      style={{ flex: 1 }}
-      enabled
-      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-    >
-      <ScrollView
-        keyboardShouldPersistTaps="handled"
-        contentContainerStyle={{ flex: 1 }}
+const SignUp: React.FC = () => {
+  const navigation = useNavigation();
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        enabled
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
       >
-        <Container>
-          <Image source={logo} />
-          <Title>Register</Title>
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logo} />
+            <Title>Register</Title>
 
-          <Input name="name" icon="user" placeholder="Name" />
-          <Input name="email" icon="mail" placeholder="Email" />
-          <Input name="password" icon="lock" placeholder="Password" />
+            <Input name="name" icon="user" placeholder="Name" />
+            <Input name="email" icon="mail" placeholder="Email" />
+            <Input name="password" icon="lock" placeholder="Password" />
 
-          <Button onPress={() => console.log('Sign in')}>Sign up</Button>
-        </Container>
-      </ScrollView>
-    </KeyboardAvoidingView>
+            <Button onPress={() => console.log('Sign in')}>Sign up</Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
 
-    <BackToSignInButton onPress={() => console.log('Sign up')}>
-      <Icon name="arrow-left" size={20} color="#fff" />
-      <BackToSignInText>Back to login</BackToSignInText>
-    </BackToSignInButton>
-  </>
-);
+      <BackToSignInButton onPress={() => navigation.goBack()}>
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <BackToSignInText>Back to login</BackToSignInText>
+      </BackToSignInButton>
+    </>
+  );
+};
 
 export default SignUp;
